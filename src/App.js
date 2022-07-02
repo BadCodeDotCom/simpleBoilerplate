33b3811ad924b5a4f57ecb363e05a6751bf3e31a
BadCodeDotCom <illegaltechno133@gmail.com>
2022-07-02 22:11:01 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SimpleNews from "./components/simpleNews";
+
+const App = () => {
+  return (
+<div className="news-generator-form">
+  <h1>News generator</h1>
+  <SimpleNews>
+
+  </SimpleNews>
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/simpleNews.js
@@ -0,0 +1,17 @@
+import './simpleNews.css';
+
+const SimpleNews = () => {
+    return <div className="simple-news-item">
+        <input type="text"/>
+        <div>
+        <button>
+            Search news
+        </button>
+        </div>
+        <h2>Latest news</h2>
+    </div>
+    
+
+}
+
+export default SimpleNews;
